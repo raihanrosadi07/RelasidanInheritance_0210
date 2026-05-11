cacf34c55fdf98f4264a1821e7fe987e78bdb851
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-11 19:04:18 +0700

    class pelajar

--- inheritance.cpp
+++ inheritance.cpp
@@ -19,3 +19,21 @@ public:
     }
 
 };
+
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+
+    ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
+
+};
