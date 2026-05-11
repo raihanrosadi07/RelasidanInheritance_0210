420858101de45f4dab8077a81887e0f9aebf31ae
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-11 18:00:08 +0700

    int main

--- Agregasi.cpp
+++ Agregasi.cpp
@@ -6,3 +6,19 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
+int main() {
+    ibu* varIbu = new ibu("rani");
+    ibu* varIbu2 = new ibu("sari");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("tiara");
+    anak* varAnak3 = new anak("dini");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    return 0;
+}
