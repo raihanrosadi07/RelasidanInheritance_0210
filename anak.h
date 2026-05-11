40b8d5ed1198585fe8962a3116fe126fb50523e6
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-11 17:45:24 +0700

    endif

--- anak.h
+++ anak.h
@@ -2,5 +2,16 @@
 #define ANAK_H
 
 class anak {
-public:      
+public:  
+       
+    string nama;
+
+    anak(string pNama) : nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada \n";
+    }
+
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
 };
+#endif
