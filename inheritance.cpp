a703b4afac8c13e3d6e88c26d000b4160fd104ce
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-11 18:59:54 +0700

    class orang

---
+++ inheritance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+    string nama;
+
+    orang(string pNama) :nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+
+    ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    }
+
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+
+};
