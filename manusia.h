4063bd3c5e50e2ee2da015a834e76c773b38af7a
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-11 18:52:00 +0700

    class manusia

--- manusia.h
+++ manusia.h
@@ -1,2 +1,16 @@
 #ifndef MANUSIA_H
 #define MANUSIA_H
+
+class manusia {
+public:
+    string name;
+    Jantung varJantung;
+
+    manusia(string pName) : name(pName) {
+        cout << name << "Hidup\n";
+    }
+    ~manusia() {
+        cout << name << "Mati\n";
+    }
+};
+#endif
