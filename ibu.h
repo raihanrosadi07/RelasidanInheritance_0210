3e2df5bc9a891b2ad40fed3f131bf58e315bcf37
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-11 17:48:58 +0700

    class ibu

---
+++ ibu.h
@@ -0,0 +1,16 @@
+#ifndef IBU_H
+#define IBU_H
+
+class anak;
+
+class ibu {
+public:
+
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "ibu \"" << nama << "\" ada\n";
+    };
+
+}
